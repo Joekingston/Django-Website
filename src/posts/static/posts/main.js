afd306bc5d22e4ce56780c87da1d68d4883d5e54
Joseph <Jcarson3035@conestogac.on.ca>
2024-03-21 14:14:38 -0400

    Added spinner and posts

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,1 +1,40 @@
 console.log('hello world')
+
+const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
+
+
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log(response)
+    },
+    error: function(error){
+        console.log('error', error)
+    },
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(element => {
+                postsBox.innerHTML += `
+                    ${element.title} - <b>${element.body}</b><br>
+                `
+            });
+        }, 500)
+
+    },
+    error: function(error){
+        console.log('error', error)
+    },
+})
